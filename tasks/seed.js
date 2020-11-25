33906aafe9aed19a501c2f25e5fb40cbc307b774
yong1996 <yque@stevens.edu>
2020-11-25 18:22:56 -0500

    1

--- tasks/seed.js
+++ tasks/seed.js
@@ -40,23 +40,29 @@ const main = async () => {
     const db = await dbConnection();
     await db.dropDatabase();
     // =================================
-    // Create a user 
-
     let originPassword1 = "a";
-
     let user1 = {
         username: 'a',
         password:originPassword1,
         avatarId: null
     }
-
     const userOne = await userData.addUser(user1.username, user1.password);
     console.log("user1 created");
-    
     let userId1 = userOne._id.toString();
     await uploadUserAvatar(userId1, 'public/img/avatar/demo/user1.jpg');
-    // await db.serverConfig.close();
 
+    let originPassword2 = "b";
+    let user2 = {
+        username: 'b',
+        password:originPassword2,
+        avatarId: null
+    }
+    const userTwo = await userData.addUser(user2.username, user2.password);
+    console.log("user2 created");
+    let userId2 = userTwo._id.toString();
+    await uploadUserAvatar(userId2, 'public/img/avatar/demo/user2.jpg');
+
+    // =================================
     question1 = await questionData.addQuestion(
         "11",
         "22",
@@ -64,8 +70,16 @@ const main = async () => {
     );
     user1question1 = question1._id.toString();
     await uploadQuestionAvatar(user1question1, 'public/img/question/dog.jpg');
-    
     console.log("a's question1 created");
+
+    // =================================
+    for (let i = 0; i < 8; i++) {
+        comment = await commentsData.addComment("You are so lovely", userId2, user1question1);
+    }
+    console.log("question1's comment created");
+
+
+    // =================================
     await db.serverConfig.close();
 }
 
